47c5eeb9cde2134a636bab6c0389434267df001a
Allen Mathews <allenjoemathews@gmail.com>
2023-05-22 13:46:17 +1000

    added prices of the crypto

--- js/script.js
+++ js/script.js
@@ -1,34 +1,33 @@
-fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.regular})`
-        document.getElementById('author').textContent = `By: ${data.user.name}`;
+        document.getElementById("author").textContent = `By: ${data.user.name}`
     })
     .catch(err => {
-        document.body.style.backgroundImage = `url('https://images.unsplash.com/photo-1518837695005-2083093ee35b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODQ3MjI0MDR8&ixlib=rb-4.0.3&q=80&w=1080')`;
-        document.getElementById('author').textContent = `By: ${data.user.name}`;
+        // Use a default background image/author
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`
+        document.getElementById("author").textContent = `By: Dodi Achmad`
     })
 
-fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        document.getElementById('crypto').textContent = `Price: ${bitcoin.usd}`
-    })
-
-fetch('https://api.coingecko.com/api/v3/coins/dogecoin')
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
     .then(res => {
         if (!res.ok) {
-            throw Error("Something went wrong!")
+            throw Error("Something went wrong")
         }
         return res.json()
     })
     .then(data => {
-        document.getElementById('crypto-top').innerHTML = ` 
-        <img src=${data.image.small} />
-        <span>${data.name}</span>
+        document.getElementById("crypto-top").innerHTML = `
+            <img src=${data.image.small} />
+            <span>${data.name}</span>
+        `
+
+        document.getElementById("crypto").innerHTML += `
+            <p>🎯: $${data.market_data.current_price.usd}</p>
+            <p>👆: $${data.market_data.high_24h.usd}</p>
+            <p>👇: $${data.market_data.low_24h.usd}</p>
         `
     })
-
-
-.catch(err => console.error(err))
+    .catch(err => console.error(err))
