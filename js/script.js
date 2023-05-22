7f97401969ce37c58c9171e60f8555bdb5cf6341
Allen Mathews <allenjoemathews@gmail.com>
2023-05-22 12:32:52 +1000

    added catch method to set default image

--- js/script.js
+++ js/script.js
@@ -4,3 +4,6 @@ fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(${data.urls.full})`
         document.getElementById('author').textContent = `By: ${data.user.name}`;
     })
+    .catch(err => {
+        document.body.style.backgroundImage = `url('https://images.unsplash.com/photo-1518837695005-2083093ee35b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODQ3MjI0MDR8&ixlib=rb-4.0.3&q=80&w=1080')`
+    })
