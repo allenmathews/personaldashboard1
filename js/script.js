2148d9fa09ca6b23f456652d5c6bf60ad9d66928
Allen Mathews <allenjoemathews@gmail.com>
2023-05-22 12:41:55 +1000

    added flex properties along with layout variation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
     .then(res => res.json())
     .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.full})`
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
         document.getElementById('author').textContent = `By: ${data.user.name}`;
     })
     .catch(err => {
-        document.body.style.backgroundImage = `url('https://images.unsplash.com/photo-1518837695005-2083093ee35b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODQ3MjI0MDR8&ixlib=rb-4.0.3&q=80&w=1080')`
+        document.body.style.backgroundImage = `url('https://images.unsplash.com/photo-1518837695005-2083093ee35b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODQ3MjI0MDR8&ixlib=rb-4.0.3&q=80&w=1080')`;
+        document.getElementById('author').textContent = `By: ${data.user.name}`;
     })
